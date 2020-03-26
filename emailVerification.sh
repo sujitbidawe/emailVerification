25ba75e34146f92390ac5c71396133f242f45f69
sujitbidawe <bidawesujit@gmail.com>
2020-03-27 01:56:34 +0530

    added: code to validate the third mandatory part--> 'abc@bridgelabz.co'

--- emailVerification.sh
+++ emailVerification.sh
@@ -2,7 +2,7 @@
 
 shopt -s extglob
 
-emailPattern="^([a-z]{1,}[0-9a-z]{0,}[@]{1}[a-z]{1,})"
+emailPattern="^([a-z]{1,}[0-9a-z]{0,}[@]{1}[a-z]{1,}[.]{1}[a-z]{2,})"
 
 read -p "enter a pin code to validate: " email
 
